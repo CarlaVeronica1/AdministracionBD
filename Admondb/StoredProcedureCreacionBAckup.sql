567946474713663bf04f1b8b5936d3f8badd5dec
CarlaVeronica1 <163350931+CarlaVeronica1@users.noreply.github.com>
2025-03-23 20:49:04 -0600

    Add files via upload

---
+++ Admondb/StoredProcedureCreacionBAckup.sql
@@ -0,0 +1,21 @@
+create or alter proc sp_backupsCompletos
+@basededatos nvarchar(50),
+@ruta nvarchar(50),
+@nombredeBackup nvarchar(50),
+@descripcion nvarchar(150)
+AS
+BEGIN
+DECLARE @SQL NVARCHAR(MAX);
+
+set @SQL='	use master;''
+     backup database'+ @basededatos+'
+	to disk ='+@ruta+' 
+	with 
+	name ='+@nombredeBackup+',
+description='+@descripcion+'
+go';
+	  PRINT @SQL;
+    -- Execute the dynamic SQL
+    EXEC sp_executesql @SQL;
+END;
+GO
