8e1287cbbbbb8f0f12204a349fc8523cb783204e
CarlaVeronica1 <163350931+CarlaVeronica1@users.noreply.github.com>
2025-03-24 07:58:30 -0600

    Update StoredProcedureCreacionBAckup.sql

--- Admondb/StoredProcedureCreacionBAckup.sql
+++ Admondb/StoredProcedureCreacionBAckup.sql
@@ -1,4 +1,6 @@
-create or alter proc sp_backupsCompletos
+use CreacionDatabase;
+
+create or alter procedure sp_backupsCompletos3
 @basededatos nvarchar(50),
 @ruta nvarchar(50),
 @nombredeBackup nvarchar(50),
@@ -7,15 +9,20 @@ AS
 BEGIN
 DECLARE @SQL NVARCHAR(MAX);
 
-set @SQL='	use master;''
-     backup database'+ @basededatos+'
-	to disk ='+@ruta+' 
+set @SQL='	use master; 
+	backup database '+ @basededatos+'
+	to disk = '''+@ruta+'/backup'+@basededatos+'.bak'' 
 	with 
-	name ='+@nombredeBackup+',
-description='+@descripcion+'
-go';
+	name = '''+@nombredeBackup+''',
+description= '''+@descripcion+'''';
 	  PRINT @SQL;
     -- Execute the dynamic SQL
     EXEC sp_executesql @SQL;
 END;
 GO
+
+exec sp_backupsCompletos3
+@basededatos ='Northwind',
+@ruta ='C:\Users\keita\OneDrive\Escritorio',
+@nombredeBackup ='Northwincito',
+@descripcion ='gjhgjh'
